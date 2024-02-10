73eacea122a4b8eee38a922f8868ba40db2dea27
Steven FU <furnbirds@gmail.com>
2024-02-10 14:55:25 +0800

    creates functions to test scope

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    
+    customerName = 'BOB';
+    
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = ""
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "John"
+    console.log(leastFavoriteCustomer)
+}
+
+
